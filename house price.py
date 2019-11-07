d8bccebd471d3c32d68ba951072da64e50dad072
Wei-Ming Ma <51375838+gtset164@users.noreply.github.com>
2019-11-07 16:19:39 +0800

    Modified skew transformation

--- house price.py
+++ house price.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# **House Prices: Advanced Regression Techniques
+# House Prices: Advanced Regression Techniques
 # 
 # This project is a compeition in Kaggle with initially 79 features for the residential homes in Ames, Iowa. The goal of this challenge is to predict the final price of each home.
 # As usual, data exploration, feature engineering are applied, then several machine learning models are built to predict the house price in the test set.
 
-# In[70]:
+# In[33]:
 
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from scipy.stats import skew
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 df_train = pd.read_csv('train.csv')
@@ -22,7 +27,7 @@ df_test = pd.read_csv('test.csv')
 
 # Functions which are used in this project. They can also be reused in other projects.
 
-# In[71]:
+# In[34]:
 
 
 # Returns a concatenated dataframe of training and test set on axis 0
@@ -51,42 +56,105 @@ def map_category(series, mapping):
     digit_data = series.map(mapping)
     return digit_data
 
-# Calculate the probability mass function for statistics
-def pmf(series, b_normal):
-    a = series.value_counts()
-    if b_normal:
-        a = a/a.sum()
-    return a
+#Function to measure accuracy.
+def rmlse(val, target):
+    return np.sqrt(np.sum(((np.log1p(val) - np.log1p(np.expm1(target)))**2) / len(target)))
 
 
 # Data Exploration
 
-# In[72]:
+# In[35]:
 
 
 print(df_train.shape)
 print(df_test.shape)
 
 
-# In[73]:
+# In[36]:
 
 
 df_train.info()
 
 
-# In[74]:
+# In[37]:
 
 
 df_train.describe(include='all')
 
 
-# In[75]:
+# In[38]:
 
 
 df_train.head()
 
 
-# In[76]:
+# In[39]:
+
+
+# Heatmap on training set including sales price to observe the Pearson correlations.
+corr = df_train.corr(method = 'pearson')
+print(corr.head())
+
+plt.figure(figsize=(12, 12))
+sns.heatmap(corr, vmax=1, vmin=-1, cmap="YlGnBu" )
+
+
+# Note:
+# 1. Large negative correlations are equaly important as large positive values in machine learning.
+# 2. Pearson correlations can only represent numerical values including categorical ordinal feature values.
+# 3. Pearson correlations only measure linear correlations. It does not work well when there is non-linear relation between two features. Therefore, there might be other relations even though the correlation values are low between some features.
+
+# 
+# The figure below can also show that there is lower correlation between lot area size and sale price.
+
+# In[40]:
+
+
+jitter = 150
+x = df_train['LotArea']
+y = df_train['SalePrice'] + np.random.uniform(-jitter, jitter, len(df_train))
+#y = df_train['SalePrice']
+
+plt.plot(x, y, 'o', markersize=5, alpha=0.1, color = 'Blue')
+# Limit Sale Price range within 50000 as only limited lot area data greater than 20000 better easier observation.
+plt.xlim(0,20000)
+# Same reason to limit sale price
+plt.ylim(0,400000)
+plt.xlabel=('Lot Area')
+plt.ylabel=('Sale Price')
+plt.show()
+
+print(corr.loc['LotArea', 'SalePrice'])
+
+
+# The figure below can also show that there is higher correlation between built year and sale price.
+
+# In[41]:
+
+
+jitter = 150
+x = df_train['YearBuilt']
+y = df_train['SalePrice'] + np.random.uniform(-jitter, jitter, len(df_train))
+
+plt.plot(x, y, 'o', markersize=5, alpha=0.08, color = 'Green')
+plt.xlim(1900,2010)
+plt.ylim(0, 500000)
+
+plt.xlabel=('YearBuilt')
+plt.ylabel=('Sale Price')
+plt.show()
+
+print(corr.loc['YearBuilt', 'SalePrice'])
+
+
+# In[42]:
+
+
+#It seems that SalePrice is skewered, so it needs to be transformed. Will do it later.
+sns.distplot(df_train['SalePrice'], kde=False, color='c', hist_kws={'alpha': 0.9})
+
+
+# In[43]:
 
 
 # Extract the house prices to a new series
@@ -105,7 +173,7 @@ print(y_train.shape)
 
 # Check the null values in training set
 
-# In[77]:
+# In[44]:
 
 
 df_nu = df_train.isnull().sum()
@@ -116,7 +184,7 @@ print(df_nu[df_nu > 0])
 
 # Check the null values in test set
 
-# In[78]:
+# In[45]:
 
 
 df_nu = df_test.isnull().sum()
@@ -127,7 +195,7 @@ print(df_nu[df_nu > 0])
 
 # There are 4 features 'Alley','PoolQC','Fence','MiscFeature' with almost all null values. Merge training and test data first then remove the 4 features.
 
-# In[79]:
+# In[46]:
 
 
 # Merge training and test set to process feature engineering together
@@ -135,7 +203,7 @@ df_all = concat_df(df_train, df_test)
 print(df_all.shape)
 
 
-# In[80]:
+# In[47]:
 
 
 df_all.drop(['Alley','PoolQC','Fence','MiscFeature'], axis = 1, inplace= True)
@@ -143,7 +211,7 @@ df_all.drop(['Alley','PoolQC','Fence','MiscFeature'], axis = 1, inplace= True)
 
 # Initially fill in the null values for all features with null values above.
 
-# In[81]:
+# In[48]:
 
 
 df_all['MSZoning'].fillna('RL', inplace=True) # test set only
@@ -190,7 +258,7 @@ df_all.isnull().sum()
 # 2. For categorical and ordinal data, map all values to numbers
 # 3. Do normalization and scaling finally for all features - including both categorical data and regression data. It's not essential (optioanl) for tree related algorithms.
 
-# In[82]:
+# In[49]:
 
 
 # Check the data summary so far
@@ -199,7 +267,7 @@ print(df_all.head())
 print(df_all.shape)
 
 
-# Use one-hot encoding to handle categorical + norminal features below:
+# Use one-hot encoding to handle categorical + norminal features below: 
 # MSZoning
 # Street
 # LotShape
@@ -229,7 +297,7 @@ print(df_all.shape)
 # SaleCondition
 # 
 
-# In[83]:
+# In[50]:
 
 
 cols = ['MSZoning', 'Street', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'Foundation', 'Heating', 'CentralAir', 'Electrical', 'Functional', 'GarageType', 'GarageFinish', 'MoSold', 'SaleType', 'SaleCondition']
@@ -241,10 +309,7 @@ df_all.drop(cols, axis = 1, inplace = True)
 print(type(onehot))
 print(onehot.shape)
 print(df_all.shape)
-
-df_all = pd.concat([df_all, onehot], axis=1)
-
-print(df_all.shape)
+df_all.info()
 
 
 # Map all the categorical ordianl features which the order of the values make sense and their values are not numerical below:
@@ -266,7 +331,7 @@ print(df_all.shape)
 # 
 # The mappings can be determined by referring the data_description file which describes the meanings of each features.
 
-# In[84]:
+# In[51]:
 
 
 mapping = {'Ex':5, 'Gd':4, 'TA':3, 'Fa':2, 'Po':1, 'NA':0, 'Av':3, 'Mn':2, 'No':1}
@@ -289,8 +354,37 @@ df_all['BsmtFinType2'] = map_category(df_all['BsmtFinType2'], mapping)
 mapping = {'Y':2, 'P':1, 'N':0}
 df_all['PavedDrive'] = map_category(df_all['PavedDrive'], mapping)
 
+# The following features are still type object but they should be numbers. Convert them to int64. 
+df_all['BsmtFinSF1'] = df_all['BsmtFinSF1'].astype(int)
+df_all['BsmtFinSF2'] = df_all['BsmtFinSF2'].astype(int)
+df_all['BsmtFullBath'] = df_all['BsmtFinSF2'].astype(int)
+df_all['BsmtHalfBath'] = df_all['BsmtFinSF2'].astype(int)
+df_all['GarageCars'] = df_all['BsmtFinSF2'].astype(int)
+df_all['MasVnrArea'] = df_all['BsmtFinSF2'].astype(int)
 
-# In[85]:
+
+# Transform skewed features.
+
+# In[52]:
+
+
+for col in df_all.columns:
+    if skew(df_all[col]) > 0.75:
+        df_all[col] = np.log1p(df_all[col])
+
+y_train = np.log1p(y_train)
+
+
+#  Merge dummy features and original data set.
+
+# In[53]:
+
+
+df_all = pd.concat([df_all, onehot], axis=1)
+print(df_all.shape)
+
+
+# In[54]:
 
 
 df_nu = df_all.isnull().sum()
@@ -299,10 +393,8 @@ df_nu = df_all.isnull().sum()
 print(df_nu[df_nu>0])
 
 
-# In[86]:
+# In[55]:
 
-
-#df_all.to_csv(r'D:\ML\Kaggle projects\House Prices Advanced Regression Techniques\check None.csv')
 
 # Check the data set dimensions so far again.
 print(df_train.shape)
@@ -310,12 +402,12 @@ print(df_all.shape)
 X_train, X_test = divide_df(df_all, df_train.shape[0])
 print(X_train.shape)
 print(X_test.shape)
-print(y_train.shape)    
+print(y_train.shape)
 
 
 # Scaling features
 
-# In[87]:
+# In[56]:
 
 
 col = X_train.columns
@@ -331,21 +423,24 @@ scaler = StandardScaler()
 X_test = pd.DataFrame(scaler.fit_transform(X_test), columns = col)
 
 
-# In[88]:
+# PCA transformation
+
+# As the heapmap shows most of the correlations between two features are low, it's possible that PCA transformation/ decomposition
+# cannot imrove performance, or even loss some information after PCA. Will still try the result to compare anyway.
+
+# In[57]:
 
 
-# TBD PCA
+pca = PCA(200, whiten=True, random_state=42).fit(X_train)
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+print(X_train.shape)
+print(type(X_train_pca))
 
 
 # Discard polynomail features due to lack of data samples comparing number of features
 
-# In[89]:
-
-
-from sklearn.model_selection import train_test_split
-
-
-# In[90]:
+# In[58]:
 
 
 from sklearn.preprocessing import PolynomialFeatures
@@ -362,108 +457,80 @@ X_train_t, X_test_t, y_train_t, y_test_t = train_test_split(X_train_poly, y_trai
 # Apply Ridge (L2) to polynomial features
 ridge = Ridge(alpha = 1000).fit(X_train_t, y_train_t)
 
-print('Score of Polynomial Ridge on training set', ridge.score(X_train_t, y_train_t))
-print('Score of Polynomial Ridge on test set', ridge.score(X_test_t, y_test_t))
+pred = np.expm1(ridge.predict(X_test_t))
+print(rmlse(pred, y_test_t))
 
 
-# In[91]:
+# In[59]:
 
 
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
+X_train_t, X_test_t, y_train_t, y_test_t = train_test_split(X_train, y_train, test_size = 0.25, random_state = 42)
 
-X_train_t, X_test_t, y_train_t, y_test_t = train_test_split(X_train, y_train, test_size = 0.3, random_state = 42)
+# For PCA features
+X_train_t_pca, X_test_t_pca, y_train_t_pca, y_test_t_pca = train_test_split(X_train_pca, y_train, test_size = 0.25, random_state = 42)
 
-print(X_train_t.shape)
-print(X_test_t.shape)
 
+# Linear Regression model with L2 regularization
+
+# In[60]:
 
 
 from sklearn.linear_model import LinearRegression
 
-
-
-#print(X_train.dtypes)
-
-#X_train.to_csv('X_train_final.csv')
-#clf = LinearRegression()
-#lr = clf.fit(X_train_t, y_train_t)
-#print('Score of Linear Regression ', lr.score(X_test_t, y_test_t))
-
-# TBD Try different alpha values and plot
 ridge = Ridge(alpha = 1200).fit(X_train_t, y_train_t)
-print('Score of Ridge on training set', ridge.score(X_train_t, y_train_t))
-print('Score of Ridge on test set', ridge.score(X_test_t, y_test_t))
+pred = np.expm1(ridge.predict(X_test_t))
+print(rmlse(pred, y_test_t))
 
 
-# Implement KNN. KNN is simple with only one parameter for tuning (k) so often be used for baseline for ML computing. However, it's not good for this case as KNN does not perform well on datasets with many features.
+# Implement Tree algorithms - Random Forest
 
-# In[92]:
+# Without PCA
 
-
-from sklearn.neighbors import KNeighborsClassifier 
-
-clf = KNeighborsClassifier(n_neighbors = 100)
-knn = clf.fit(X_train_t, y_train_t)
-print('Score of knn on training set', knn.score(X_train_t, y_train_t))
-print('Score of knn on test set', knn.score(X_test_t, y_test_t))
-
-
-# As expected, the performace result is poor for KNN. Therefore, it won't be applied and just for reference here.
-
-# Implement Tree algorithms
-
-# In[93]:
-
-
-print(X_train_t.shape)
-print(X_train_t.head())
-
-
-# In[94]:
+# In[61]:
 
 
 from sklearn.ensemble import RandomForestRegressor
 
 clf = RandomForestRegressor(max_depth = 20, random_state = 42, n_estimators = 100)
 rf = clf.fit(X_train_t, y_train_t)
-print('Score of Kernel Random Forest on training set', rf.score(X_train_t, y_train_t))
-print('Score of Kernel Random Forest on test set', rf.score(X_test_t, y_test_t))
+pred = np.expm1(rf.predict(X_test_t))
+print(rmlse(pred, y_test_t))
 
 
-# In[97]:
+# With PCA
+
+# In[62]:
 
 
-# Compare predictions on test samples
-pred = rf.predict(X_test_t)
-print(pred.shape)
-print(pred)
-
-print(y_test_t)
+clf = RandomForestRegressor(max_depth = 20, random_state = 42, n_estimators = 100)
+rf = clf.fit(X_train_t_pca, y_train_t_pca)
+pred = np.expm1(rf.predict(X_test_t_pca))
+print(rmlse(pred, y_test_t_pca))
 
 
-# In[101]:
+# The result for PCA meet the earlier guess - PCA does not help to improve the performance in this project, so will not apply PCA.
+
+# 
+# XG-Boost algorithm
+
+# In[63]:
 
 
-pred = rf.predict(X_test)
-np.savetxt('D:\ML\Kaggle projects\House Prices Advanced Regression Techniques\predict.csv', pred, delimiter=',')
-#pred.tofile('D:\ML\Kaggle projects\House Prices Advanced Regression Techniques\predict.csv')
+import xgboost as xgb
+
+clf = xgb.XGBRegressor(n_estimators=340, max_depth=2, learning_rate=0.1)
+xgb = clf.fit(X_train_t, y_train_t)
+#score=cross_val_score(clf, X_train, y_train, cv=k_fold)
+
+pred = np.expm1(xgb.predict(X_test_t))
+rmlse(pred, y_test_t)
 
 
-# Implement Kernel SVM (Support Vector Machine). Normally it performs much better when features are scaled and when there are many features.
-
-# In[96]:
+# In[64]:
 
 
-from sklearn.svm import SVR
-
-svm = SVR(kernel = 'rbf', C=1, gamma = 1).fit(X_train_t, y_train_t)
-print('Score of Kernel SVM on training set', svm.score(X_train_t, y_train_t))
-print('Score of Kernel SVM on test set', svm.score(X_test_t, y_test_t))
+pred = np.expm1(xgb.predict(X_test))
+np.savetxt('D:\ML\Kaggle projects\House Prices Advanced Regression Techniques\predict.csv', pred)
 
 
-# In[ ]:
-
-
-
-
+# The score (RMSLE) based on XG-Boost in this file is 0.13515 on Kaggle.
